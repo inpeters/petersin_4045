aa488b47d59fa1159109ad79732ecc8a89270a1e
inpeters <75335175+inpeters@users.noreply.github.com>
2022-09-06 14:51:22 -0400

    Add Javadoc, add default / endpoint

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -3,12 +3,22 @@ package com.plantplaces;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Handle the /start end point
+ * return
+ */
 @Controller 
 public class PlantPlacesController {
 	
 	
+	
 	@RequestMapping("/start")
 	public String start() {
+		return "start";
+	}
+	
+	@RequestMapping("/")
+	public String index() {
 		return "start";
 	}
 
